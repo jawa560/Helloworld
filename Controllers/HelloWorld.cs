a7e89a01d8906748c4d5918434c240310cfb7879
James Wang <59338529+jawa560@users.noreply.github.com>
2024-10-04 01:52:18 +0800

    加入專案檔案。

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Helloworld.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET: api/<HelloWorldController>
+        [HttpGet]
+        public string Get()
+        {
+            return  "Hello World!";
+        }
+        // GET api/<HelloWorldController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        // POST api/<HelloWorldController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        // PUT api/<HelloWorldController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        // DELETE api/<HelloWorldController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
